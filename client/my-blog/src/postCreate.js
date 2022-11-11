e0c3234ed805f83a50b69cdcbae788eb726af260
Adeyemi <yemiklein77@gmail.com>
2022-11-11 23:04:54 +0100

    new commit

---
+++ client/my-blog/src/postCreate.js
@@ -0,0 +1,16 @@
+/* eslint-disable import/no-anonymous-default-export */
+import React from "react";
+
+
+export default () => {
+    return <div>
+        <form>
+            <div className="form-group">
+                <label>Title</label>
+                <input className="form-control" />
+                </div> 
+                <button className="btn btn-primary">Submit</button>
+        </form>
+    </div>;
+
+}
