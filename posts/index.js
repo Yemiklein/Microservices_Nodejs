34df2c077be70015460c65d54dbb4c38887309ed
Adeyemi <yemiklein77@gmail.com>
2022-11-11 21:16:51 +0100

    first commit

---
+++ posts/index.js
@@ -0,0 +1,37 @@
+const express = require ('express');
+const {randomBytes} = require('crypto');
+const bodyParser = require('body-parser');
+const router = express.Router();
+const app = express ()
+const port = 5555;
+const posts = {};
+
+
+
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get('/posts', (req, res) => {
+    res.send(posts);
+});
+
+
+app.post('/posts', (req, res) => {
+    const id = randomBytes(4).toString('hex');
+    const {title} = req.body;
+    posts[id] = {
+        id, 
+        title,
+    };
+    res.status(201).json(posts[id]);
+});
+
+
+
+
+
+
+app.listen(port, () =>{
+    console.log(`Server is running on port ${port}`);
+})
