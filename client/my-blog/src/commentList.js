389116074263917b8b30d2cfda0d32c1a1b4f79f
Adeyemi <yemiklein77@gmail.com>
2022-11-13 19:43:05 +0100

    commentlist

---
+++ client/my-blog/src/commentList.js
@@ -0,0 +1,29 @@
+/* eslint-disable import/no-anonymous-default-export */
+import React, {useState, useEffect} from "react";
+import axios from "axios";
+
+
+export default () => {
+    const [comments, setComments] = useState({});
+    const fetchData = async () => {
+        const res = await axios.get("http://localhost:5000/posts");
+        setComments(res.data);
+    };
+    useEffect(() => {
+        fetchData();
+    }, []);
+    const renderedComments = comments.map((comment) => {
+        return (
+       <li
+         key={comment.id}
+       >
+              {comment.content}
+       </li>
+        );
+    });
+    return (
+       <ul>
+              {renderedComments}
+       </ul>
+    );
+}
--- client/my-blog/src/postList.js
+++ client/my-blog/src/postList.js
@@ -2,7 +2,7 @@
 import React, {useState, useEffect} from 'react';  
 import axios from 'axios'; 
 import CommentCreate from './commentCreate';
-
+import CommentList from './commentList';
 
 
 export default () => {
@@ -22,6 +22,7 @@ export default () => {
              >
                 <div className="card-body">
                     <h3>{post.title}</h3>
+                    <CommentList postId={post.id} />
                     <CommentCreate postId={post.id} />
                 </div>
             </div>
